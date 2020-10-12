a96646423142e074b8699e829f6f69c772501ba5
Joan Rieu <joan@joanrieu.com>
2020-10-12 12:07:38 -0400

    add calendar with month filter

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,19 @@
 import configJson from "../config.json";
 
 export interface Config {
-  accounts: Record<
-    string,
-    {
-      type: "debit" | "credit";
-      files: string[];
-    }
-  >;
-  groups: Record<
-    string,
-    Record<
-      string,
-      {
-        icon: string;
-        income?: true;
-      }
-    >
-  >;
+  accounts: Record<string, Account>;
+  groups: Record<string, Record<string, Category>>;
 }
+
+export type Account = {
+  type: "debit" | "credit";
+  files: string[];
+};
+
+export type Category = {
+  icon: string;
+  income?: true;
+};
 
 const config: Config = configJson as any;
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import _ from "lodash";
-import { observable } from "mobx";
+import { filter, groupBy, sumBy } from "lodash";
+import { makeAutoObservable, runInAction } from "mobx";
+import { observer } from "mobx-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import transactions from "../transactions.json";
-import config from "./config";
+import config, { Category } from "./config";
 
 type Transaction = typeof transactions[0];
 
@@ -36,20 +37,30 @@ function getAccount(account: string) {
 }
 
 class UI {
-  @observable month = new Date("2020-09").toISOString().slice(0, 8);
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  year = new Date().getFullYear();
+  month = new Date().getMonth() + 1;
+
+  get datePrefix() {
+    return this.year + "-" + this.month.toString().padStart(2, "0") + "-";
+  }
 }
 
 const ui = new UI();
 
-const App = () => (
+const App = observer(() => (
   <div>
     <Title />
+    <Calendar />
     <CategoryList />
     <TransactionList />
   </div>
-);
+));
 
-const Title = () => (
+const Title = observer(() => (
   <h1
     style={{
       textAlign: "center",
@@ -62,108 +73,146 @@ const Title = () => (
   >
     Budget
   </h1>
+));
+
+const Section = observer(
+  ({ name, children }: { name: string; children: React.ReactNode }) => (
+    <div>
+      <h2
+        style={{
+          textAlign: "center",
+          fontSize: "130%",
+          letterSpacing: ".2ch",
+          padding: "16px",
+        }}
+      >
+        {name}
+      </h2>
+      {children}
+    </div>
+  )
 );
 
-const Section = ({
-  name,
-  children,
-}: {
-  name: string;
-  children: React.ReactNode;
-}) => (
-  <div>
-    <h2
+const Subsection = observer(
+  ({ name, children }: { name: string; children: React.ReactNode }) => (
+    <div>
+      <h3
+        style={{
+          textAlign: "center",
+          textTransform: "uppercase",
+          fontSize: "80%",
+          letterSpacing: ".2ch",
+          color: "grey",
+          background: "#ddd",
+          border: "1px solid #bbb",
+          borderWidth: "1px 0",
+          padding: "16px",
+        }}
+      >
+        {name}
+      </h3>
+      {children}
+    </div>
+  )
+);
+
+const Calendar = observer(() => (
+  <Section name="Calendar">
+    <form
       style={{
-        textAlign: "center",
-        fontSize: "130%",
-        letterSpacing: ".2ch",
-        padding: "16px",
+        display: "grid",
+        gridTemplateColumns: "1fr 96px 64px 1fr",
+        gap: "16px",
       }}
+      onSubmit={(event) => event.preventDefault()}
     >
-      {name}
-    </h2>
-    {children}
-  </div>
-);
+      <input
+        type="number"
+        name="year"
+        defaultValue={ui.year}
+        onChange={(event) =>
+          runInAction(() => (ui.year = Number(event.target.value)))
+        }
+        style={{ fontSize: "150%", gridColumn: "2" }}
+      />
+      <input
+        type="number"
+        name="month"
+        min={1}
+        max={12}
+        defaultValue={ui.month}
+        onChange={(event) =>
+          runInAction(() => (ui.month = Number(event.target.value)))
+        }
+        style={{ fontSize: "150%", gridColumn: "3" }}
+      />
+    </form>
+  </Section>
+));
 
-const Subsection = ({
-  name,
-  children,
-}: {
-  name: string;
-  children: React.ReactNode;
-}) => (
-  <div>
-    <h3
-      style={{
-        textAlign: "center",
-        textTransform: "uppercase",
-        fontSize: "80%",
-        letterSpacing: ".2ch",
-        color: "grey",
-        background: "#ddd",
-        border: "1px solid #bbb",
-        borderWidth: "1px 0",
-        padding: "16px",
-      }}
-    >
-      {name}
-    </h3>
-    {children}
-  </div>
-);
-
-const CategoryList = () => (
+const CategoryList = observer(() => (
   <Section name="Categories">
     {Object.entries(config.groups).map(
       ([name, group]) =>
         !isGroupExcluded(name) && (
           <Subsection key={name} name={name}>
             {Object.entries(group).map(([name, category]) => (
-              <div
-                key={name}
-                style={{
-                  padding: "16px 32px",
-                  borderBottom: "1px solid #bbb",
-                  marginBottom: "-1px",
-                  display: "grid",
-                  gridTemplateColumns: "16px 1fr auto",
-                  gap: "16px",
-                  lineHeight: "1",
-                }}
-              >
-                <div style={{ placeSelf: "center" }}>
-                  {getCategoryIcon(name)}
-                </div>
-                <div style={{ placeSelf: "center start" }}>{name}</div>
-                <div style={{ placeSelf: "center end" }}>
-                  {new Intl.NumberFormat(navigator.language, {
-                    style: "currency",
-                    currency: "CAD",
-                    signDisplay: "exceptZero",
-                  } as Intl.NumberFormatOptions).format(
-                    _.sumBy(
-                      _.filter(
-                        transactions,
-                        (transaction) =>
-                          transaction.Category === name &&
-                          transaction.Date.startsWith(ui.month)
-                      ),
-                      (transaction) => transaction.Amount
-                    )
-                  )}
-                </div>
-              </div>
+              <CategoryLine key={name} name={name} category={category} />
             ))}
           </Subsection>
         )
     )}
   </Section>
+));
+
+const CategoryLine = observer(
+  ({ name, category }: { name: string; category: Category }) => {
+    const activity = sumBy(
+      filter(
+        transactions,
+        (transaction) =>
+          transaction.Category === name &&
+          transaction.Date.startsWith(ui.datePrefix)
+      ),
+      (transaction) => transaction.Amount
+    );
+    return (
+      <div
+        style={{
+          borderBottom: "1px solid #bbb",
+          marginBottom: "-1px",
+        }}
+      >
+        <div
+          style={{
+            padding: "16px 32px",
+            display: "grid",
+            gridTemplateColumns: "16px 1fr auto",
+            gap: "16px",
+            lineHeight: "1",
+            opacity: activity ? undefined : "0.3",
+          }}
+        >
+          <div style={{ placeSelf: "center" }}>{getCategoryIcon(name)}</div>
+          <div style={{ placeSelf: "center start" }}>{name}</div>
+          <div style={{ placeSelf: "center end" }}>
+            {new Intl.NumberFormat(navigator.language, {
+              style: "currency",
+              currency: "CAD",
+              signDisplay: "exceptZero",
+            } as Intl.NumberFormatOptions).format(activity)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 );
 
-const TransactionList = () => {
-  const byDate = _.groupBy(
-    transactions.filter((transaction) => getAccount(transaction.Account)),
+const TransactionList = observer(() => {
+  const byDate = groupBy(
+    filter(transactions, (transaction) =>
+      transaction.Date.startsWith(ui.datePrefix)
+    ),
     "Date"
   );
   const dates = Object.keys(byDate).sort().reverse();
@@ -171,6 +220,7 @@ const TransactionList = () => {
     day: "numeric",
     month: "long",
     year: "numeric",
+    timeZone: "UTC",
   });
   return (
     <Section name="Transactions">
@@ -183,66 +233,68 @@ const TransactionList = () => {
       ))}
     </Section>
   );
-};
+});
 
-const TransactionLine = ({ transaction }: { transaction: Transaction }) => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateAreas: '"icon payee amount" "icon category amount"',
-      gridTemplateColumns: "auto 1fr auto",
-      opacity: isCategoryExcluded(transaction.Category) ? "0.3" : "1",
-      padding: "16px",
-    }}
-  >
+const TransactionLine = observer(
+  ({ transaction }: { transaction: Transaction }) => (
     <div
       style={{
-        gridArea: "icon",
-        placeSelf: "center",
-        border: "1px solid grey",
-        borderRadius: "100%",
-        width: "48px",
-        height: "48px",
-        color: "grey",
-        background: "lightgrey",
-        fontSize: "150%",
         display: "grid",
+        gridTemplateAreas: '"icon payee amount" "icon category amount"',
+        gridTemplateColumns: "auto 1fr auto",
+        opacity: isCategoryExcluded(transaction.Category) ? "0.3" : "1",
+        padding: "16px",
       }}
     >
       <div
         style={{
+          gridArea: "icon",
           placeSelf: "center",
-          lineHeight: "0",
+          border: "1px solid grey",
+          borderRadius: "100%",
+          width: "48px",
+          height: "48px",
+          color: "grey",
+          background: "lightgrey",
+          fontSize: "150%",
+          display: "grid",
         }}
       >
-        {getCategoryIcon(transaction.Category)}
+        <div
+          style={{
+            placeSelf: "center",
+            lineHeight: "0",
+          }}
+        >
+          {getCategoryIcon(transaction.Category)}
+        </div>
       </div>
+      <div
+        style={{
+          gridArea: "payee",
+          placeSelf: "end start",
+          margin: "0 16px",
+          textTransform: "capitalize",
+        }}
+      >
+        {transaction.Payee.toLowerCase().replace(/[^a-z0-9]/g, " ")}
+      </div>
+      <div
+        style={{
+          gridArea: "category",
+          placeSelf: "start start",
+          fontSize: "80%",
+          color: "grey",
+          margin: "0 16px",
+        }}
+      >
+        {transaction.Category}
+      </div>
+      <pre style={{ gridArea: "amount", placeSelf: "center" }}>
+        {transaction.Amount.toFixed(2)}
+      </pre>
     </div>
-    <div
-      style={{
-        gridArea: "payee",
-        placeSelf: "end start",
-        margin: "0 16px",
-        textTransform: "capitalize",
-      }}
-    >
-      {transaction.Payee.toLowerCase().replace(/[^a-z0-9]/g, " ")}
-    </div>
-    <div
-      style={{
-        gridArea: "category",
-        placeSelf: "start start",
-        fontSize: "80%",
-        color: "grey",
-        margin: "0 16px",
-      }}
-    >
-      {transaction.Category}
-    </div>
-    <pre style={{ gridArea: "amount", placeSelf: "center" }}>
-      {transaction.Amount.toFixed(2)}
-    </pre>
-  </div>
+  )
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
